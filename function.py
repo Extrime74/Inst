06baa3a542d8bdb72b9127866ed4b0e7ecd7f3be
Extrime92 <94184789+Extrime92@users.noreply.github.com>
2023-10-03 18:11:18 +0300

    Changes

--- function.py
+++ function.py
@@ -1,5 +1,4 @@
-import time
-
+from threading import Timer
 from instagrapi.exceptions import LoginRequired
 from instagrapi.exceptions import UnknownError
 from instagrapi import Client
@@ -29,6 +28,7 @@ def login_user():
                 print('Сессия активна')
             except LoginRequired:
                 logger.info('Session is invalid, need to login via username and password')
+                print('Session is invalid, need to login via username and password')
                 old_session = cl.get_settings()
 
                 # use the same device uuids across logins
@@ -38,28 +38,34 @@ def login_user():
             login_via_session = True
         except Exception as e:
             logger.info("Couldn't login user using session information: %s" % e)
+            print("Couldn't login user using session information: %s" % e)
 
     if not login_via_session:
         try:
             logger.info('Attempting to login via username and password. username: %s' % config.USERNAME)
+            print('Attempting to login via username and password. username: %s' % config.USERNAME)
             if cl.login(config.USERNAME, config.PASSWORD):
                 login_via_pw = True
         except Exception as e:
             logger.info("Couldn't login user using username and password: %s" % e)
+            print("Couldn't login user using username and password: %s" % e)
 
     if not login_via_pw and not login_via_session:
+        print("Couldn't login user with either password or session")
         raise Exception("Couldn't login user with either password or session")
-def session_login():
-    cl.load_settings('session.json')
-    cl.login(config.USERNAME, config.PASSWORD)
-    cl.get_timeline_feed()  # check session
-def inst_login():                                 # Функция логина в инстаграм.
-    login_user()                                  # Логинимся при запуске бота.
-    cl.delay_range = [1, 4]
-    # session_login()
-    cl.get_timeline_feed()                        # Обновляем ленту, чтобы замаскироваться под реального человека.
-    name = cl.account_info().full_name            # Получаем имя акканта в инстаграм.
-    print(f'Добро пожаловать в аккаунт "{name}"')
+
+
+# def session_login():
+#     cl.load_settings('session.json')
+#     cl.login(config.USERNAME, config.PASSWORD)
+#     cl.get_timeline_feed()  # check session
+# def inst_login():
+#     login_user()
+#     cl.delay_range = [1, 4]
+#     cl.get_timeline_feed()
+#     name = cl.account_info().full_name
+#     print(f'Добро пожаловать в аккаунт "{name}"')
+
 def photo_cleanup():
     try:
         os.remove('image.jpg')
@@ -74,48 +80,48 @@ def photo_upload_feed():
     try:
         cl.photo_upload('image.jpg', caption='')
         cl.get_timeline_feed()
-        print('The photo was posted to feed')
+        print('Фото загружено в ленту')
         return 'OK'
     except UnknownError:
         cl.get_timeline_feed()
-        print('Something went wrong')
+        print('Что-то пошло не так')
         pass
 def photo_upload_story():
     try:
         cl.photo_upload_to_story('image.jpg', caption='')
         cl.get_timeline_feed()
-        print('The photo was posted to story')
+        print('Фото загружено в сторис')
         return 'OK'
     except UnknownError:
         cl.get_timeline_feed()
-        print('Something went wrong')
+        print('Что-то пошло не так')
         pass
 def video_upload_feed():
     try:
         cl.video_upload('video.mp4', caption='')
         cl.get_timeline_feed()
-        print('The video was posted to feed')
+        print('Видео загружено в ленту')
         return 'OK'
     except UnknownError:
         cl.get_timeline_feed()
-        print('Something went wrong')
+        print('Что-то пошло не так')
         pass
 def video_upload_story():
     try:
         cl.video_upload_to_story('video.mp4', caption='')
         cl.get_timeline_feed()
-        print('The video was posted to story')
+        print('Видео загружено в сторис')
         return 'OK'
     except UnknownError:
         cl.get_timeline_feed()
-        print('Something went wrong')
+        print('Что-то пошло не так')
         pass
 
-def get_current_time():
-    t = time.localtime()
-    current_time = time.strftime('%H:%M:%S', t)
-    print(current_time)
-    return current_time
+# def get_current_time():
+#     t = time.localtime()
+#     current_time = time.strftime('%H:%M:%S', t)
+#     print(current_time)
+#     return current_time
 
 def wait_for_time(self):
     while True:
@@ -126,3 +132,8 @@ def wait_for_time(self):
             continue
         else:
             pass
+
+def refresh():
+    cl.get_timeline_feed()
+    Timer(400, refresh).start()
+    print('Обновлено')
