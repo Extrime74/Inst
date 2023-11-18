192c6eee02b54df1773e248cc414a6077db1eda4
Extrime92 <94184789+Extrime92@users.noreply.github.com>
2023-11-18 22:20:22 +0300

    Changes

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -1,17 +1,21 @@
-import random
-import time
 import telebot
 from telebot import types
 import config
-import function
 from pathlib import Path
+import logging
 from instagrapi import Client
+from instagrapi.exceptions import LoginRequired
 import os
+import datetime
 
 
 Path(f'Download/Photo/').mkdir(parents=True, exist_ok=True)
 Path(f'Download/Video/').mkdir(parents=True, exist_ok=True)
-
+cl = Client()
+logger = logging.getLogger()
+current_date = datetime.datetime.now()
+date = current_date.strftime('%d.%m.%y %H:%M')
+r = types.ReplyKeyboardRemove()
 
 class PostingBot:
     def __init__(self):
@@ -19,14 +23,55 @@ class PostingBot:
         self.fileid = None
         self.token = config.TOKEN
         self.bot = telebot.TeleBot(self.token)
-        r = types.ReplyKeyboardRemove()
 
-        print('Запусти бота в Telegram')
+        print('\n#### Запусти бота в Telegram ####')
+
+        def login_telebot():
+            session = cl.load_settings('session.json')
+            login_via_session = False
+            login_via_pw = False
+
+            if session:
+                try:
+                    cl.set_settings(session)
+                    cl.login(config.USERNAME, config.PASSWORD)
+
+                    # check if session is valid
+                    try:
+                        cl.get_timeline_feed()
+                        print("\n####", date, "####", "\nСессия активна\n")
+                    except LoginRequired:
+                        logger.info('Session is invalid, need to login via username and password')
+                        print('Session is invalid, need to login via username and password')
+                        old_session = cl.get_settings()
+
+                        # use the same device uuids across logins
+                        cl.set_settings({})
+                        cl.set_uuids(old_session['uuids'])
+                        cl.login(config.USERNAME, config.PASSWORD)
+                    login_via_session = True
+                except Exception as e:
+                    logger.info("Couldn't login user using session information: %s" % e)
+                    print("Couldn't login user using session information: %s" % e)
+
+            if not login_via_session:
+                try:
+                    logger.info('Attempting to login via username and password. username: %s' % config.USERNAME)
+                    print('Attempting to login via username and password. username: %s' % config.USERNAME)
+                    if cl.login(config.USERNAME, config.PASSWORD):
+                        login_via_pw = True
+                except Exception as e:
+                    logger.info("Couldn't login user using username and password: %s" % e)
+                    print("Couldn't login user using username and password: %s" % e)
+
+            if not login_via_pw and not login_via_session:
+                print("Couldn't login user with either password or session")
+                raise Exception("Couldn't login user with either password or session")
 
         @self.bot.message_handler(commands=['start'])
         def start_command(message):
             if message.chat.id == config.MY_USER_ID:
-                function.login_user()
+                login_telebot()
                 self.bot.send_message(message.from_user.id, f'👋 Привет, {message.from_user.first_name}!'
                                                             f'\nПоделись со мной фото или видео и я загружу их '
                                                             f'в твой инстаграм!')
@@ -35,34 +80,38 @@ class PostingBot:
                 def send_welcome(ping_message):
                     self.bot.send_message(ping_message.from_user.id, 'Я тут')
 
-                @self.bot.message_handler(content_types=['audio', 'voice', 'document', 'text', 'location', 'contact', 
+                @self.bot.message_handler(content_types=['audio', 'voice', 'document', 'text', 'location', 'contact',
                                                          'sticker', 'video_note'])
                 def others(types_message):
                     if types_message.content_type == 'voice':
                         self.bot.send_message(types_message.chat.id, 'Ну все, ты огребаешь!')
                         self.bot.send_sticker(types_message.chat.id, 'CAACAgIAAxkBAAEKcaplHG8ULx1nbcG6DDvnJyIqxX'
                                                                      '-iFQAC5hMAAi8iyEvZrfyv1izExzAE')
-                    if types_message.content_type == 'audio':
+                    elif types_message.content_type == 'audio':
                         self.bot.send_message(types_message.chat.id, 'Ну и херню же ты слушаешь')
-                    if types_message.content_type == 'document':
+                    elif types_message.content_type == 'document':
                         self.bot.send_message(types_message.chat.id, 'Ты где это взял?')
-                    if types_message.content_type == 'text':
+                    elif types_message.content_type == 'text':
                         self.bot.send_message(types_message.chat.id, 'Ну ты ваще')
-                    if types_message.content_type == 'location':
+                    elif types_message.content_type == 'location':
                         self.bot.send_message(types_message.chat.id, 'Опять где-то шляешься!')
-                    if types_message.content_type == 'contact':
+                    elif types_message.content_type == 'contact':
                         self.bot.send_message(types_message.chat.id, 'Кореш твой?')
-                    if types_message.content_type == 'sticker':
+                    elif types_message.content_type == 'sticker':
                         self.bot.send_sticker(types_message.chat.id, 'CAACAgIAAxkBAAEKcahlHGuJCzjjAtM'
                                                                      'EpdK5ayCoL6NAnQACJhoAApmoyEtA7xJ_hrc3ajAE')
-                    if types_message.content_type == 'video_note':
+                    elif types_message.content_type == 'video_note':
                         self.bot.send_sticker(types_message.chat.id, 'CAACAgIAAxkBAAEKcahlHGuJCzjjAtM'
                                                                      'EpdK5ayCoL6NAnQACJhoAApmoyEtA7xJ_hrc3ajAE')
 
                 @self.bot.message_handler(content_types=['photo'])
                 def photo(photo_message):
+                    try:
+                        os.remove('image.jpg')
+                    except Exception as e:
+                        print("####", date, "####", e)
+                        pass
 
-                    function.photo_cleanup()
                     self.fileid = photo_message.photo[-1].file_id
                     self.file_info = self.bot.get_file(self.fileid)
                     self.downloaded_file = self.bot.download_file(self.file_info.file_path)
@@ -81,22 +130,30 @@ class PostingBot:
                 def photo_text(photo_text_message):
 
                     if photo_text_message.text == 'Выложить фото в ленту':
-                        status = function.photo_upload_feed()
-                        if status == 'OK':
+                        try:
+                            cl.photo_upload('image.jpg', caption='')
+                            cl.get_timeline_feed()
+                            print("####", date, "####", "\nФото загружено в ленту\n")
                             self.bot.send_message(photo_text_message.from_user.id, 'Запостил', reply_markup=r)
-                        else:
-                            self.bot.send_message(photo_text_message.from_user.id, 'Что-то пошло не так.',
-                                                  reply_markup=r)
+                        except Exception as e:
+                            cl.get_timeline_feed()
+                            print("####", date, "####", e)
+                            self.bot.send_message(photo_text_message.from_user.id, e, reply_markup=r)
+                            pass
 
-                    if photo_text_message.text == 'Выложить фото в сторис':
-                        status = function.photo_upload_story()
-                        if status == 'OK':
+                    elif photo_text_message.text == 'Выложить фото в сторис':
+                        try:
+                            cl.photo_upload_to_story('image.jpg', caption='')
+                            cl.get_timeline_feed()
+                            print("####", date, "####", "\nФото загружено в сторис\n")
                             self.bot.send_message(photo_text_message.from_user.id, 'Запостил', reply_markup=r)
-                        else:
-                            self.bot.send_message(photo_text_message.from_user.id, 'Что-то пошло не так.',
-                                                  reply_markup=r)
+                        except Exception as e:
+                            cl.get_timeline_feed()
+                            print("####", date, "####", e)
+                            self.bot.send_message(photo_text_message.from_user.id, e, reply_markup=r)
+                            pass
 
-                    if photo_text_message.text == 'Сохранить в хранилище':
+                    elif photo_text_message.text == 'Сохранить в хранилище':
                         src = f'Download/Photo/' + str(photo_text_message.date) + '.jpg'
                         with open(src, 'wb') as new_file:
                             new_file.write(self.downloaded_file)
@@ -105,7 +162,11 @@ class PostingBot:
                 @self.bot.message_handler(content_types=['video'])
                 def video(video_message):
 
-                    function.video_cleanup()
+                    try:
+                        os.remove('video.mp4')
+                    except Exception as e:
+                        print("####", date, "####", e)
+                        pass
                     self.fileid = video_message.video.file_id
                     self.file_info = self.bot.get_file(self.fileid)
                     self.downloaded_file = self.bot.download_file(self.file_info.file_path)
@@ -124,26 +185,30 @@ class PostingBot:
                 def video_text(video_text_message):
 
                     if video_text_message.text == 'Выложить видео в ленту':
-                        status = function.video_upload_feed()
-                        if status == 'OK':
-                            self.bot.send_message(video_text_message.from_user.id, 'Запостил.', reply_markup=r)
-                        else:
-                            self.bot.send_message(video_text_message.from_user.id,
-                                                  'Что-то пошло не так.'
-                                                  '\nВозможно, видео слишком длинное.',
-                                                  reply_markup=r)
+                        try:
+                            cl.video_upload('video.mp4', caption='')
+                            cl.get_timeline_feed()
+                            print("####", date, "####", "\nВидео загружено в ленту\n")
+                            self.bot.send_message(video_text_message.from_user.id, 'Запостил', reply_markup=r)
+                        except Exception as e:
+                            cl.get_timeline_feed()
+                            print("####", date, "####", e)
+                            self.bot.send_message(video_text_message.from_user.id, e, reply_markup=r)
+                            pass
 
-                    if video_text_message.text == 'Выложить видео в сторис':
-                        status = function.video_upload_story()
-                        if status == 'OK':
-                            self.bot.send_message(video_text_message.from_user.id, 'Запостил.', reply_markup=r)
-                        else:
-                            self.bot.send_message(video_text_message.from_user.id,
-                                                  'Что-то пошло не так.'
-                                                  '\nВозможно, видео слишком длинное.',
-                                                  reply_markup=r)
+                    elif video_text_message.text == 'Выложить видео в сторис':
+                        try:
+                            cl.video_upload_to_story('video.mp4', caption='')
+                            cl.get_timeline_feed()
+                            print("####", date, "####", "\nВидео загружено в сторис\n")
+                            self.bot.send_message(video_text_message.from_user.id, 'Запостил', reply_markup=r)
+                        except Exception as e:
+                            cl.get_timeline_feed()
+                            print("####", date, "####", e)
+                            self.bot.send_message(video_text_message.from_user.id, e, reply_markup=r)
+                            pass
 
-                    if video_text_message.text == 'Сохранить в хранилище':
+                    elif video_text_message.text == 'Сохранить в хранилище':
                         src = f'Download/Video/' + str(video_text_message.date) + '.mp4'
                         with open(src, 'wb') as new_file:
                             new_file.write(self.downloaded_file)
