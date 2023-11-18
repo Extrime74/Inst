82187fa64113028eeccf7fbee952b3a5eb40cbb5
Extrime92 <94184789+Extrime92@users.noreply.github.com>
2023-11-18 22:25:57 +0300

    Changes

--- make_session.py
+++ make_session.py
@@ -1,6 +1,14 @@
 from instagrapi import Client
 import config
+import datetime
 
+
+current_date = datetime.datetime.now()
+date = current_date.strftime('%d.%m.%y %H:%M')
 cl = Client()
+
 cl.login(config.USERNAME, config.PASSWORD)
 cl.dump_settings("session.json")
+
+
+print("####",date,"####", f"\nФайл сессии сохранен")
