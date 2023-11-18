76220c2b3ee30bef696cc9828bba2262425a80a7
Extrime92 <94184789+Extrime92@users.noreply.github.com>
2023-11-18 22:40:26 +0300

    Changes

--- make_session_autopost.py
+++ make_session_autopost.py
@@ -5,10 +5,10 @@ import datetime
 
 current_date = datetime.datetime.now()
 date = current_date.strftime('%d.%m.%y %H:%M')
-cl = Client(proxy=config.PROXY)
+cl = Client()
 
 cl.login(config.USERNAME, config.PASSWORD)
 cl.dump_settings("session_autopost.json")
 
 
-print("####",date,"####", f"\nФайл сессии autopost сохранен")
+print("####", date, "####", f"\nФайл сессии autopost сохранен")
